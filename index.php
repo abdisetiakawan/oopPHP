c4f711069c10291b5ee387fc949c134c8f9f117e
abdisetiakawan <bedikadiryt@gmail.com>
2024-08-12 22:21:54 +0700

    visibility implementation

--- index.php
+++ index.php
@@ -2,9 +2,19 @@
 class Produk {
     public $judul,
            $penulis,
-           $penerbit,
-           $harga;
+           $penerbit;
+    private $harga;
 
+
+    /**
+     * Initializes a new instance of the Produk class.
+     *
+     * @param string $judul The title of the product.
+     * @param string $penulis The author of the product.
+     * @param string $penerbit The publisher of the product.
+     * @param int $harga The price of the product.
+     * @return void
+     */
     public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
         $this->judul = $judul;
         $this->penulis = $penulis;
@@ -12,24 +22,53 @@ class Produk {
         $this->harga = $harga;
     }
 
+    /**
+     * Mengembalikan label produk dalam bentuk string.
+     *
+     * @return string
+     */
     public function getLabel() {
         return "$this->penulis, $this->penerbit";
     }
 
+    /**
+     * Mengembalikan informasi produk dalam bentuk string.
+     *
+     * @return string
+     */
     public function getInfoProduk() {  
         $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
         return $str;
     }
+    public function getHarga() {
+        return $this->harga;
+    }
+
 }
 
 class Komik extends Produk {
     public $jmlHalaman;
 
+    /**
+     * Initializes a new instance of the Komik class.
+     *
+     * @param string $judul The title of the komik.
+     * @param string $penulis The author of the komik.
+     * @param string $penerbit The publisher of the komik.
+     * @param int $harga The price of the komik.
+     * @param int $jmlHalaman The number of pages in the komik.
+     * @return void
+     */
     public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0) {
         parent::__construct($judul, $penulis, $penerbit, $harga);
         $this->jmlHalaman = $jmlHalaman;
     }
 
+    /**
+     * Mengembalikan informasi produk komik dalam bentuk string.
+     *
+     * @return string
+     */
     public function getInfoProduk() {
         $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
         return $str;
@@ -39,15 +78,36 @@ class Komik extends Produk {
 class Game extends Produk {
     public $waktuMain;
 
+    /**
+     * Initializes a new instance of the Game class.
+     *
+     * @param string $judul The title of the game.
+     * @param string $penulis The author of the game.
+     * @param string $penerbit The publisher of the game.
+     * @param int $harga The price of the game.
+     * @param int $waktuMain The duration of the game in minutes.
+     * @return void
+     */
     public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0) {
         parent::__construct($judul, $penulis, $penerbit, $harga);
         $this->waktuMain = $waktuMain;
     }
 
+    /**
+     * Mengembalikan informasi produk game dalam bentuk string.
+     *
+     * @return string
+     */
     public function getInfoProduk() {
         $str = "Game : " . parent::getInfoProduk() . " - {$this->waktuMain} Jam.";
         return $str;
     }
+    /**
+     * Mengembalikan harga produk.
+     *
+     * @return int
+     */
+
 }
 
 class CetakInfoProduk {
@@ -67,8 +127,19 @@ echo $produk2->getInfoProduk();
 
 
 
-
 echo "<hr>";
-$cetakInfo = new CetakInfoProduk();
-echo $cetakInfo->cetak($produk1);
+echo $produk2->getHarga();
+/**
+ * Access Modifier (Pengatur Akses) dalam OOP (Object-Oriented Programming)
+ *
+ * - Public: Atribut atau method yang dapat diakses dari mana saja, baik dari dalam kelas itu sendiri maupun dari luar kelas.
+ * - Protected: Atribut atau method yang dapat diakses dari dalam kelas itu sendiri dan dari kelas turunannya (subclass).
+ * - Private: Atribut atau method yang hanya dapat diakses dari dalam kelas itu sendiri dan tidak dapat diakses dari luar kelas atau kelas turunannya.
+ */
+
+
+
+
+//$cetakInfo = new CetakInfoProduk();
+//echo $cetakInfo->cetak($produk1);
 ?>
